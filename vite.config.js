9b8be3fb2f536144020a6d439e8c72211025ed43
frsman1745-tech <frs.man1745@gmail.com>
2026-05-19 22:35:00 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
+    target: "es2022", // <--- هذا هو السطر الذي قمنا بإضافته لحل مشكلة الـ Top-level await
     rollupOptions: {
       // Split vendor chunks for better caching
       output: {
